cfc69e5ba5d2a969db1529cda8002de3b920f7ee
grodiscostas@hotmail.com <grodiscostas@hotmail.com>
2019-05-27 11:33:12 +0300

    Added Hello

---
+++ View_Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+
+namespace View_Console
+{
+    class Program
+    {
+        private const string Value = "dfrff";
+
+        static void Main(string[] args)
+        {
+            Console.Write("Hello!!!");
+            Console.Write("Katoikia:");
+            string b = Console.ReadLine();
+            Console.Write("Hlikia:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("H Hlikia mou einai: {0}",a);
+
+            Console.WriteLine($"H ilikia mou einai {a} kai katoikw ston {b}");
+        }
+    }
+}
